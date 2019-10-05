3507b3d672bb62df4531ddc1fab481d8c1598ba4
nredjap <hinanias@gmail.com>
2019-10-05 17:12:20 +0900

    add タスクのdataClassを追加

--- app/src/main/java/com/nanoyatsu/todoapp/data/entity/Task.kt
+++ app/src/main/java/com/nanoyatsu/todoapp/data/entity/Task.kt
@@ -1,4 +1,12 @@
 package com.nanoyatsu.todoapp.data.entity
 
-class Task {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "label") val label: String,
+    @ColumnInfo(name = "completed") val completed: Boolean
+)
