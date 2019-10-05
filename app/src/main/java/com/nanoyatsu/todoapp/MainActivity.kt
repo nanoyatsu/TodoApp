6abc2d2d6760e101e22c1955d334b4053a4db8d0
nredjap <hinanias@gmail.com>
2019-10-05 21:22:46 +0900

    update タスク追加ボタン：DBと接続して稼働するようになった

--- app/src/main/java/com/nanoyatsu/todoapp/MainActivity.kt
+++ app/src/main/java/com/nanoyatsu/todoapp/MainActivity.kt
@@ -2,9 +2,13 @@ package com.nanoyatsu.todoapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.nanoyatsu.todoapp.data.TodoDatabase
 import com.nanoyatsu.todoapp.data.entity.Task
 import com.nanoyatsu.todoapp.view.taskList.TabViewAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         val tasks = getTasks()
         tabViewAdapter = TabViewAdapter(supportFragmentManager, tasks)
         list_container.adapter = tabViewAdapter
+
+        task_add_button.setOnClickListener { addTask(task_add_label.text?.toString()) }
     }
 
     private fun getTasks(): ArrayList<Task> {
@@ -26,5 +32,21 @@ class MainActivity : AppCompatActivity() {
         tasks.add(Task(4, "dummy-4-true", true))
         tasks.add(Task(5, "dummy-5-false", false))
         return tasks
+    }
+
+    private fun addTask(taskLabel: String?) {
+        if (taskLabel.isNullOrEmpty()) {
+            Snackbar
+                .make(snackbar_coordinator, "Please input your task.", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+            return
+        }
+
+        runBlocking(Dispatchers.IO) {
+            TodoDatabase.getInstance().taskDao().insert(Task(label = taskLabel, completed = false))
+        }
+        task_add_label.text?.clear()
+        task_add_label.clearFocus()
     }
 }
