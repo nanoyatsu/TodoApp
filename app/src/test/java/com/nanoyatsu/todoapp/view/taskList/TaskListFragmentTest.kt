553bf5621e9493d15fc7cdcd713661b09c822b07
nredjap <hinanias@gmail.com>
2019-10-18 23:09:05 +0900

    add Fragmentのテスト模索中 保持している値にアクセスできた（たぶん）

--- app/src/test/java/com/nanoyatsu/todoapp/view/taskList/TaskListFragmentTest.kt
+++ app/src/test/java/com/nanoyatsu/todoapp/view/taskList/TaskListFragmentTest.kt
@@ -2,11 +2,12 @@ package com.nanoyatsu.todoapp.view.taskList
 
 import android.os.Build
 import android.os.Bundle
-import androidx.core.content.MimeTypeFilter.matches
-import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.android.synthetic.main.fragment_task_list.*
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -27,11 +28,13 @@ class TaskListFragmentTest {
     @Test
     fun testEventFragment() {
         val fragmentArgs = Bundle().apply {
-//            putInt("selectedListItem", 0) // ここでbundle指定
+            //            putInt("selectedListItem", 0) // ここでbundle指定
         }
 //        val factory = // FragmentFactoryわからない・・・（調べる）
         val scenario = launchFragmentInContainer<TaskListFragment>(fragmentArgs)
-
-
+        scenario.moveToState(Lifecycle.State.CREATED)
+        scenario.onFragment {
+            Assert.assertEquals(it.recycler_list.adapter?.itemCount, 0)
+        }
     }
 }
